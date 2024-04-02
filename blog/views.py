16d2763266425b16a71cd2e248565fe7ee890c69
Park-SangWook <qkrtkddnr0118@gmai.com>
2024-04-02 17:30:23 +0900

    HTML site added
    + extra options

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+# 이 함수는 요청을 넘겨받아 render 메서드를 호출한다.
+# 이 render 메서드는 요청을 받아 blog/post_list.html 템플릿을 보여준다.
